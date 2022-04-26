77137e835b770b4a84aac96ee91b957df1f06534
wang shengpeng <spwang0280@gmail.com>
2022-04-26 15:46:33 +0900

    Quiz5

---
+++ Ass-05-2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 10:08:45 2022
+
+@author: sp
+"""
+
+
+a =int(input('enter tan integer:'))
+b = int(input('enter tan integer:'))
+c = int(input('enter tan integer:'))
+d = int(input('enter tan integer:'))
+
+e =[a,b,c,d]
+f = sorted(set(e),reverse=True)
+f.remove(max(f))
+print("the max is",max(e))
+print(f)
